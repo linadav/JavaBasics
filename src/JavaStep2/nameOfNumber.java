ddb00e3f3a861fbded1b40cd21f02500fe47e711
linadav <lina_dav@yahoo.co.uk>
2019-12-07 15:31:54 +0200

    initial commit to JavaStep2

--- src/JavaStep2/nameOfNumber.java
+++ src/JavaStep2/nameOfNumber.java
@@ -1,4 +1,16 @@
 package JavaStep2;
 
+import java.util.Scanner;
+
 public class nameOfNumber {
+    public static void main(String[] args){
+        // isveskite skaiciaus pavadinima
+        String[] skaicius =  {"nulis", "vienas", "du", "trys", "keturi","penki", "sesi", "septyni","astuoni", "devyni"};
+
+        Scanner numIn = new Scanner(System.in);
+        System.out.println("Iveskite skaiciu nuo 0 iki 9: ");
+        int num = numIn.nextInt();
+
+        System.out.println("Tai yra " + skaicius[num] );
+    }
 }
