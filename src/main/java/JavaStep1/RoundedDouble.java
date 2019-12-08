6a33f48ce3c0d9f9266696a34c40a03243c813ef
linadav <lina_dav@yahoo.co.uk>
2019-12-08 11:33:26 +0200

    JavaStep1 to master

---
+++ src/main/java/JavaStep1/RoundedDouble.java
@@ -0,0 +1,12 @@
+package JavaStep1;
+import java.util.Scanner;
+
+public class RoundedDouble {
+    public static void main(String[] args) {
+        Scanner numberScanner = new Scanner(System.in);
+        System.out.println("iveskite skaiciu: xxxx,xxxx");
+        double number = numberScanner.nextDouble();
+
+        System.out.printf("Ivedete skaiciu: %.2f", number);
+    }
+}
